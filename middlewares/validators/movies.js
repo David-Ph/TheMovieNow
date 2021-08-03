998ff0f11a510567be312bc3da848825dec903a6
David <davidphang431@gmail.com>
2021-08-03 15:06:51 +0700

    Fix validators for movie and change title to not be unique

--- middlewares/validators/movies.js
+++ middlewares/validators/movies.js
@@ -37,25 +37,25 @@ exports.movieValidator = async (req, res, next) => {
   try {
     const errorMessages = [];
 
-    if (validator.isEmpty(req.body.title)) {
+    if (req.body.title && validator.isEmpty(req.body.title)) {
       errorMessages.push("Title cannot be empty!");
     }
 
-    if (!validator.isDate(req.body.releaseDate)) {
+    if (req.body.releaseDate && !validator.isDate(req.body.releaseDate)) {
       errorMessages.push(
         "Release date not valid! Please enter a date in YYYY-MM-DD format"
       );
     }
 
-    if (!validator.isInt(req.body.budget)) {
+    if (req.body.budget && !validator.isInt(req.body.budget)) {
       errorMessages.push("Budget not valid! Please enter a number");
     }
 
-    if (!validator.isURL(req.body.trailer)) {
+    if (req.body.trailer && !validator.isURL(req.body.trailer)) {
       errorMessages.push("Please insert valid url!");
     }
     //  check for duplicates in req.body.categories
-    if (hasDuplicates(req.body.categories)) {
+    if (req.body.categories.length > 0 && hasDuplicates(req.body.categories)) {
       errorMessages.push(
         "Please do not insert a single category more than once!"
       );
