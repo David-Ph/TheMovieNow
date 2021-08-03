584a6b165e51b879f3accd62e96e15574bd392ce
David <davidphang431@gmail.com>
2021-08-03 21:11:30 +0700

    Fix seeders

--- seeders/index.js
+++ seeders/index.js
@@ -1,34 +1,12 @@
 const { addMovies, deleteMovies } = require("./movies");
-
-async function add() {
-  await addMovies();
-}
-
-async function remove() {
-  await deleteMovies();
-}
-
-if (process.argv[2] === "add") {
-  add().then(() => {
-    console.log("Seeders success");
-    process.exit(0);
-  });
-} else if (process.argv[2] === "remove") {
-  remove().then(() => {
-    console.log("Delete data success");
-    process.exit(0);
-  });
-}
-
-//User
 const { addUsers, deleteUsers } = require("./user");
 
 async function add() {
-  await addUsers();
+  await Promise.all([addMovies(), addUsers()]);
 }
 
 async function remove() {
-  await deleteUsers();
+  await Promise.all([deleteMovies(), deleteUsers()]);
 }
 
 if (process.argv[2] === "add") {
